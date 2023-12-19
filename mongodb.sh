c3dc446e60c6c2b5e3e3301b263db7d23b877252
Afroz <afrozalikhan81@gmail.com>
2023-12-19 12:10:35 +0530

    Roboshop nginx script

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@ echo -e "\e[33mCopy Mongodb repo file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstalling Mongodb Server\e[0m"
-dnf install mongodb-org -y &>>/tmp/roboshop.log
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mUpdate Mongodb Listen Address\e[0m"
 sed -i -e  's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
