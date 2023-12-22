4e280f89e267a91060f47c8a1b6d65578f473ac6
Afroz <afrozalikhan81@gmail.com>
2023-12-22 16:17:38 +0530

    RabbitMQ

--- cart.sh
+++ cart.sh
@@ -1,44 +1,42 @@
-component=cart
-color="\e[36m"
-nocolor="\e[0m"
-log_file="/tmp/roboshop.log"
-app_path="/app"
+source common.sh
+component=${component}
 
 
 
 
-echo -e "\e[33mConfiguration NodeJS Repos\e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstall NodeJS\e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
+echo -e "${color}Configuration NodeJS Repos ${nocolor}"
+dnf module disable nodejs -y &>>${log_file}
+dnf module enable nodejs:18 -y &>>${log_file}
 
-echo -e "\e[33mAdd Application User\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+echo -e "${color}Install NodeJS${nocolor}"
+dnf install nodejs -y &>>${log_file}
 
-echo -e "\e[33mCreate Application Directory \e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app
+echo -e "${color}Add Application User${nocolor}"
+useradd roboshop &>>${log_file}
 
-echo -e "\e[33mDownload  application Content\e[0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
-cd /app
+echo -e "${color}Create Application Directory ${nocolor}"
+rm -rf ${app_path} &>>${log_file}
+mkdir ${app_path}
 
-
-
-echo -e "\e[33mExtract Application Content\e[0m"
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
-cd /app
-
-echo -e "\e[33mInstall NodeJS Dependencies\e[0m"
-npm install &>>/tmp/roboshop.log
+echo -e "${color}Download  application Content${nocolor}"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+cd ${app_path}
 
 
-echo -e "\e[33mSetup SystemD Service\e[0m"
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
 
-echo -e "\e[33mStart cart Service\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable cart &>>/tmp/roboshop.log
-systemctl restart cart &>>/tmp/roboshop.log
+echo -e "${color}Extract Application Content${nocolor}"
+unzip /tmp/${component}.zip &>>${log_file}
+cd ${app_path}
+
+echo -e "${color}Install NodeJS Dependencies${nocolor}"
+npm install &>>${log_file}
+
+
+echo -e "${color}Setup SystemD Service${nocolor}"
+cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+
+echo -e "${color}Start ${component} Service${nocolor}"
+systemctl daemon-reload &>>${log_file}
+systemctl enable ${component} &>>${log_file}
+systemctl restart ${component} &>>${log_file}
