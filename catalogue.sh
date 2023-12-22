f7198becb4accb080d68dea913b203838d166922
Afroz <afrozalikhan81@gmail.com>
2023-12-22 18:21:37 +0530

    RabbitMQ

--- catalogue.sh
+++ catalogue.sh
@@ -1,14 +1,6 @@
 source common.sh
 component=catalogue
 
-
 nodejs
 
-echo -e "${color} Installing Nginx Server ${nocolor}"
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
-
-echo -e "${color} Copy MongoDB Repo File ${nocolor}"
-dnf install mongodb-org-shell -y &>>$log_file
-
-echo -e "${color} Load Schema ${nocolor}"
-mongo --host mongodb-dev.afroz1.online <${app_path}/schema/$component.js &>>$log_file
+mongo_schema_setup
