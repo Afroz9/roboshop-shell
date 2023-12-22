4042b698656ab170271be30909b1a352264d5538
Afroz <afrozalikhan81@gmail.com>
2023-12-22 18:55:45 +0530

    RabbitMQ

--- common.sh
+++ common.sh
@@ -1,8 +1,43 @@
 
 color="\e[35m"
-nocolor="\e[0m"
-log_file="/tmp/roboshop.log"
+nocolor="${nocolor}"
+log_file="&>>$log_file"
 app_path="/app"
+
+app_presetup(){
+  echo -e "${color} Add Application User ${nocolor}"
+    useradd roboshop &>>$log_file
+
+    echo -e "${color}Create Application Directory ${nocolor}"
+      rm -rf /app &>>$log_file
+      mkdir /app &>>$log_file
+
+      echo -e "${color} Create Application Directory ${nocolor}"
+        rm -rf ${app_path} &>>$log_file
+        mkdir ${app_path}
+
+
+      echo -e "${color}Download application Content ${nocolor}"
+        curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+
+
+    echo -e "${color} Extract Application Content ${nocolor}"
+    cd ${app_path}
+    unzip /tmp/$component.zip &>>$log_file
+
+    systemd_setup(){
+      echo -e "${color}Setup SystemD File ${nocolor}"
+        cp /home/centos/roboshop-shell/shipping.services /etc/systemd/system/shipping.service &>>$log_file
+
+        echo -e "${color}Start Shipping Service Maven ${nocolor}"
+        systemctl daemon-reload &>>$log_file
+        systemctl enable shipping &>>$log_file
+        systemctl restart shipping &>>$log_file
+    }
+
+
+
+}
 
 nodejs() {
   echo -e "${color} Configuration NodeJS Repos ${nocolor}"
@@ -12,34 +47,20 @@ nodejs() {
   echo -e "${color} Install NodeJS ${nocolor}"
   dnf install nodejs -y &>>$log_file
 
-  echo -e "${color} Add Application User ${nocolor}"
-  useradd roboshop &>>$log_file
 
-  echo -e "${color} Create Application Directory ${nocolor}"
-  rm -rf ${app_path} &>>$log_file
-  mkdir ${app_path}
+app_presetup
+
+
 
   echo -e "${color} Download  application Content ${nocolor}"
   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
-  cd ${app_path}
-
-
-
-  echo -e "${color} Extract Application Content ${nocolor}"
-  unzip /tmp/$component.zip &>>$log_file
   cd ${app_path}
 
   echo -e "${color} Install NodeJS Dependencies ${nocolor}"
   npm install &>>$log_file
 
 
-  echo -e "${color} Setup SystemD Service ${nocolor}"
-  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
-
-  echo -e "${color} Start $component Service ${nocolor}"
-  systemctl daemon-reload &>>$log_file
-  systemctl enable $component &>>$log_file
-  systemctl restart $component &>>$log_file
+  systemd_setup
 
 }
 mongo_schema_setup(){
@@ -51,4 +72,28 @@ mongo_schema_setup(){
 
   echo -e "${color} Load Schema ${nocolor}"
   mongo --host mongodb-dev.afroz1.online <${app_path}/schema/$component.js &>>$log_file
+
+}
+mysql_schema_setup(){
+  echo -e "${color}Install Maven Client ${nocolor}"
+    dnf install mysql -y &>>$log_file
+
+    echo -e "${color}Load Schema ${nocolor}"
+    mysql -h mysql-dev.afroz1.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>$log_file
+}
+
+
+maven(){
+  echo -e "${color}Install Maven ${nocolor}"
+  dnf install maven -y &>>$log_file
+  
+ app_presetup
+
+  echo -e "${color}Download Maven dependencies ${nocolor}"
+  mvn clean package &>>$log_file
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
+  
+  mysql_schema_setup
+  
+  systemd_setup
 }
--- shipping.sh
+++ shipping.sh
@@ -1,35 +1,8 @@
-echo -e "\e[33mInstall Maven \e[0m"
-dnf install maven -y &>>/tmp/roboshop.log
+source common.sh
+commopent=shipping
 
-echo -e "\e[33mAdd application User \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+maven
 
-echo -e "\e[33mCreate Application Directory \e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
 
-echo -e "\e[33mDownload application Content \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
 
-echo -e "\e[33mExtract Application Content \e[0m "
-cd /app
-unzip /tmp/shipping.zip &>>/tmp/roboshop.log
-
-echo -e "\e[33mDownload Maven dependencies \e[0m"
-mvn clean package &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
-
-echo -e "\e[33mInstall Maven Client \e[0m"
-dnf install mysql -y &>>/tmp/roboshop.log
-
-echo -e "\e[33mLoad Schema \e[0m"
-mysql -h mysql-dev.afroz1.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
-
-echo -e "\e[33mSetup SystemD File \e[0m"
-cp /home/centos/roboshop-shell/shipping.services /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
-
-echo -e "\e[33mStart Shipping Service Maven \e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl restart shipping &>>/tmp/roboshop.log
 
