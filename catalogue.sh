00b443577102c6281968ee4be163701c412673b4
Afroz <afrozalikhan81@gmail.com>
2023-12-22 12:38:41 +0530

    RabbitMQ

--- catalogue.sh
+++ catalogue.sh
@@ -1,47 +1,49 @@
 component=catalogue
+color="\e[33m"
+nocolor="\e[0m"
 
 
-echo -e "\e[33mConfiguration NodeJS Repos\e[0m"
+echo -e "${color} Configuration NodeJS Repos" ${nocolor}"
 dnf module disable nodejs -y &>>/tmp/roboshop.log
 dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstall NodeJS\e[0m"
+echo -e "${color} Install NodeJS${nocolor}"
 dnf install nodejs -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mAdd Application User\e[0m"
+echo -e "${color} Add Application User${nocolor}"
 useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[33mCreate Application Directory \e[0m"
+echo -e "${color} Create Application Directory ${nocolor}"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
-echo -e "\e[33mDownload  application Content\e[0m"
+echo -e "${color} Download  application Content${nocolor}"
 curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/roboshop.log
 cd /app
 
 
 
-echo -e "\e[33mExtract Application Content\e[0m"
+echo -e "${color} Extract Application Content${nocolor}"
 unzip /tmp/$component.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -e "\e[33mInstall NodeJS Dependencies\e[0m"
+echo -e "${color} Install NodeJS Dependencies${nocolor}"
 npm install &>>/tmp/roboshop.log
 
 
-echo -e "\e[33mSetup SystemD Service\e[0m"
+echo -e "${color} Setup SystemD Service${nocolor}"
 cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>/tmp/roboshop.log
 
-echo -e "\e[33mStart $component Service\e[0m"
+echo -e "${color} Start $component Service${nocolor}"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable $component &>>/tmp/roboshop.log
 systemctl restart $component &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstalling Nginx Server\e[0m"
+echo -e "${color} Installing Nginx Server${nocolor}"
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-echo -e "\e[33mCopy MongoDB Repo File\e[0m"
+echo -e "${color}Copy MongoDB Repo File${nocolor}"
 dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
 
-echo -e "\e[33mLoad Schema\e[0m"
+echo -e "${color} Load Schema${nocolor}"
 mongo --host mongodb-dev.afroz1.online </app/schema/$component.js &>>/tmp/roboshop.log
