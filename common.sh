ea3d255e4af45852ec8548d08fe5f6886c9a073d
Afroz <afrozalikhan81@gmail.com>
2023-12-23 15:59:17 +0530

    RabbitMQ

--- common.sh
+++ common.sh
@@ -5,14 +5,14 @@ log_file="&>>$log_file"
 app_path="/app"
 
 app_presetup(){
-  echo -e "${color} Add Application User ${nocolor}"
+  echo -e "${color}Add Application User ${nocolor}"
     useradd roboshop &>>$log_file
 
     echo -e "${color}Create Application Directory ${nocolor}"
       rm -rf /app &>>$log_file
       mkdir /app &>>$log_file
 
-      echo -e "${color} Create Application Directory ${nocolor}"
+      echo -e "${color}Create Application Directory ${nocolor}"
         rm -rf ${app_path} &>>$log_file
         mkdir ${app_path}
 
@@ -21,7 +21,7 @@ app_presetup(){
         curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
 
 
-    echo -e "${color} Extract Application Content ${nocolor}"
+    echo -e "${color}Extract Application Content ${nocolor}"
     cd ${app_path}
     unzip /tmp/$component.zip &>>$log_file
 
@@ -40,11 +40,11 @@ app_presetup(){
 }
 
 nodejs() {
-  echo -e "${color} Configuration NodeJS Repos ${nocolor}"
+  echo -e "${color}Configuration NodeJS Repos ${nocolor}"
   dnf module disable nodejs -y &>>$log_file
   dnf module enable nodejs:18 -y &>>$log_file
 
-  echo -e "${color} Install NodeJS ${nocolor}"
+  echo -e "${color}Install NodeJS ${nocolor}"
   dnf install nodejs -y &>>$log_file
 
 
@@ -52,11 +52,11 @@ app_presetup
 
 
 
-  echo -e "${color} Download  application Content ${nocolor}"
+  echo -e "${color}Download  application Content ${nocolor}"
   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
   cd ${app_path}
 
-  echo -e "${color} Install NodeJS Dependencies ${nocolor}"
+  echo -e "${color}Install NodeJS Dependencies ${nocolor}"
   npm install &>>$log_file
 
 
@@ -64,13 +64,13 @@ app_presetup
 
 }
 mongo_schema_setup(){
-  echo -e "${color} Installing Nginx Server ${nocolor}"
+  echo -e "${color}Installing Nginx Server ${nocolor}"
   cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 
-  echo -e "${color} Copy MongoDB Repo File ${nocolor}"
+  echo -e "${color}Copy MongoDB Repo File ${nocolor}"
   dnf install mongodb-org-shell -y &>>$log_file
 
-  echo -e "${color} Load Schema ${nocolor}"
+  echo -e "${color}Load Schema ${nocolor}"
   mongo --host mongodb-dev.afroz1.online <${app_path}/schema/$component.js &>>$log_file
 
 }
