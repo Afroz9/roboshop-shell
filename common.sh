a12cc9c0232f5c1ecaa4061722e40a7e154f9fe3
Afroz <afrozalikhan81@gmail.com>
2023-12-26 14:57:40 +0530

    RabbitMQ

--- common.sh
+++ common.sh
@@ -5,11 +5,11 @@ log_file="&>>$log_file"
 app_path="/app"
 
 app_presetup(){
-  echo -e "${color}Add Application User ${nocolor}"
+  echo -e "${color} Add Application User ${nocolor}"
     useradd roboshop &>>$log_file
     echo $?
 
-    echo -e "${color}Create Application Directory ${nocolor}"
+    echo -e "${color} Create Application Directory ${nocolor}"
       rm -rf /app &>>$log_file
       mkdir /app &>>$log_file
       echo $?
@@ -17,22 +17,22 @@ app_presetup(){
 
 
 
-      echo -e "${color}Download application Content ${nocolor}"
+      echo -e "${color} Download application Content ${nocolor}"
         curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
       echo $?
 
-    echo -e "${color}Extract Application Content ${nocolor}"
+    echo -e "${color} Extract Application Content ${nocolor}"
     cd ${app_path}
     unzip /tmp/$component.zip &>>$log_file
     echo $?
 }
 
     systemd_setup() {
-      echo -e "${color}Setup SystemD File ${nocolor}"
+      echo -e "${color} Setup SystemD File ${nocolor}"
         cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
         echo $?
 
-        echo -e "${color}Start Shipping Service Maven ${nocolor}"
+        echo -e "${color} Start Shipping Service Maven ${nocolor}"
         systemctl daemon-reload &>>$log_file
         systemctl enable $component &>>$log_file
         systemctl restart $component &>>$log_file
@@ -41,53 +41,53 @@ app_presetup(){
 }
 
 nodejs() {
-  echo -e "${color}Configuration NodeJS Repos ${nocolor}"
+  echo -e "${color} Configuration NodeJS Repos ${nocolor}"
   dnf module disable nodejs -y &>>$log_file
   dnf module enable nodejs:18 -y &>>$log_file
 
-  echo -e "${color}Install NodeJS ${nocolor}"
+  echo -e "${color} Install NodeJS ${nocolor}"
   dnf install nodejs -y &>>$log_file
 
 app_presetup
 
 
-  echo -e "${color}Download  application Content ${nocolor}"
+  echo -e "${color} Download  application Content ${nocolor}"
   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
   cd ${app_path}
 
-  echo -e "${color}Install NodeJS Dependencies ${nocolor}"
+  echo -e "${color} Install NodeJS Dependencies ${nocolor}"
   npm install &>>$log_file
 
   systemd_setup
 
 }
 mongo_schema_setup(){
-  echo -e "${color}Installing Nginx Server ${nocolor}"
+  echo -e "${color} Installing Nginx Server ${nocolor}"
   cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 
-  echo -e "${color}Copy MongoDB Repo File ${nocolor}"
+  echo -e "${color} Copy MongoDB Repo File ${nocolor}"
   dnf install mongodb-org-shell -y &>>$log_file
 
-  echo -e "${color}Load Schema ${nocolor}"
+  echo -e "${color} Load Schema ${nocolor}"
   mongo --host mongodb-dev.afroz1.online <${app_path}/schema/$component.js &>>$log_file
 
 }
 mysql_schema_setup() {
-  echo -e "${color}Install Maven Client ${nocolor}"
+  echo -e "${color} Install Maven Client ${nocolor}"
     dnf install mysql -y &>>$log_file
 
-    echo -e "${color}Load Schema ${nocolor}"
+    echo -e "${color} Load Schema ${nocolor}"
     mysql -h mysql-dev.afroz1.online -uroot -pRoboShop@1 </app/schema/${component}.sql &>>$log_file
 }
 
 
 maven(){
-  echo -e "${color}Install Maven ${nocolor}"
+  echo -e "${color} Install Maven ${nocolor}"
   dnf install maven -y &>>$log_file
   
  app_presetup
 
-  echo -e "${color}Download Maven dependencies ${nocolor}"
+  echo -e "${color} Download Maven dependencies ${nocolor}"
   mvn clean package &>>$log_file
   mv target/${component}-1.0.jar ${component}.jar &>>$log_file
   
@@ -97,14 +97,14 @@ maven(){
 }
 
 python(){
-  echo -e "${color}Install Python 3.6 ${nocolor}"
+  echo -e "${color} Install Python 3.6 ${nocolor}"
   dnf install python36 gcc python3-devel -y &>>/tmp/roboshop.log
   echo $?
 
 
  app_presetup
 
-  echo -e "${color}Install Application dependencies ${nocolor}"
+  echo -e "${color} Install Application dependencies ${nocolor}"
   cd /app
   pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
   echo $?
