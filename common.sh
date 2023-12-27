01ad0f1edde0e880319441bc92cd6ea9382bc4ee
Afroz <afrozalikhan81@gmail.com>
2023-12-27 11:28:52 +0530

    RabbitMQ

--- common.sh
+++ common.sh
@@ -39,7 +39,7 @@ stat_check $?
     systemd_setup() {
       echo -e "${color} Setup SystemD File ${nocolor}"
         cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
-        sed -i -e "s/roboshop_app_password/$roboshop_app_password/" /home/centos/roboshop-shell/$component.service
+        sed -i -e "s/roboshop_app_password/$roboshop_app_password/" /etc/systemd/system/$component.service
         stat_check $?
 
         echo -e "${color} Start Shipping Service Maven ${nocolor}"
