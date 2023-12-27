fd8c633696a6025280f7bcfcb1bc5019d36dac64
Afroz <afrozalikhan81@gmail.com>
2023-12-27 10:29:58 +0530

    RabbitMQ

--- common.sh
+++ common.sh
@@ -7,7 +7,7 @@ app_path="/app"
 app_presetup() {
   echo -e "${color} Add Application User ${nocolor}"
   id roboshop &>>$log_file
-  if [ $? -eq to 1 ]; then
+  if [ $? -eq  1 ]; then
     useradd roboshop &>>$log_file
   fi
 
