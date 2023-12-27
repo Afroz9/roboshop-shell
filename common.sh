d56f2978da14c3100a4010568f27e3126c3fe2d9
Afroz <afrozalikhan81@gmail.com>
2023-12-27 11:17:30 +0530

    RabbitMQ

--- common.sh
+++ common.sh
@@ -116,6 +116,9 @@ python(){
   cd /app
   pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
   stat_check $?
+
+  sed -i -e "s/roboshop_app_password/$1/" /home/centos/roboshop-shell/$component.service
+
   systemd_setup
 
 }
--- mysql.sh
+++ mysql.sh
@@ -14,4 +14,4 @@ systemctl restart mysqld &>>/tmp/roboshop.log
 
 
 echo -e "\e[33m Setup MySQL  Password \e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
+mysql_secure_installation --set-root-pass $1 &>>/tmp/roboshop.log
