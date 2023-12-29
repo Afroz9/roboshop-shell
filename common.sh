dd8092ca819df5c764e078f44849a9cc69f312bf
Afroz <afrozalikhan81@gmail.com>
2023-12-29 16:40:33 +0530

    Common services

--- common.sh
+++ common.sh
@@ -43,7 +43,7 @@ app_presetup() {
 systemd_setup() {
   echo -e "${color} Setup SystemD Service  ${nocolor}"
   cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>$log_file
-  sed -i -e "s/roboshop_app_password/$roboshop_app_password/"  /etc/systemd/system/$component.service
+  sed -i -e "s/roboshop_app_password/$roboshop_app_password/"  /etc/systemd/system/$component.service &>>$log_file
   stat_check $?
 
   echo -e "${color} Start $component Service ${nocolor}"
